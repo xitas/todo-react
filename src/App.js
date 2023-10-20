2a8fb95c4e0773a1b5f91a9a4202d294fdc386c3
xitas <xitas.ameer9@gmail.com>
2023-10-20 19:38:41 +0500

    added request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import './App.css';
 import { useState, useEffect } from "react";
 import axios from 'axios';
+const axiosinstence = axios.create(
+  { baseURL: 'http://localhost:8000/' }
+)
+
+const config = {
+  headers: {
+    "Access-Control-Allow-Origin": "http://localhost:8000/",
+  }
+}
 
 function App() {
   const [task, setTask] = useState([]);
 
-  const fetchTaskData = async () => {
-    const result = await axios.get('http://localhost:5000/api/data');
-    console.log(result.data);
-    setTask(result.data);
+
+  const getTasks = async () => {
+    const result = await axiosinstence.get('tasks', {}, config).then(
+      (response) => {
+        console.log(response)
+        setTask(response.data)
+      }
+    )
+  };
+
+  const addTask = async () => {
+    const result = await axiosinstence.post('tasks', { title: 'This is a title', description: 'This is an example of discription' }).then(
+      (response) => {
+        console.log(response)
+      }
+    )
+  };
+  const updateTask = async () => {
+    const result = await axiosinstence.put('tasks/1/', { title: 'This is updated title', description: 'This is an updated example of discription', status: 'working' }).then(
+      (response) => {
+        console.log(response)
+      }
+    )
+  };
+  const deleteTask = async () => {
+    const result = await axiosinstence.get('tasks/2/').then(
+      (response) => {
+        console.log(response)
+      } 
+    )
   };
 
   useEffect(() => {
-    fetchTaskData();
+    // addTask();
+    // getTasks();
   }, [])
-  
+
   return (
     <>
+
     </>
   );
 }
