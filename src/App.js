824f14077f33c389f4b10e0ca019e450f029d4aa
xitas <xitas.ameer9@gmail.com>
2023-10-20 19:03:10 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
+  const [task, setTask] = useState([]);
+
+  const fetchTaskData = async () => {
+    const result = await axios.get('http://localhost:5000/api/data');
+    console.log(result.data);
+    setTask(result.data);
+  };
+
+  useEffect(() => {
+    fetchTaskData();
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
